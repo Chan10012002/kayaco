4a3cf7c613d1dc9405155263535eb90b0eb18cfb
Chan10012002 <christian.obispo.5494@gmail.com>
2025-09-04 00:36:56 +0800

    fix:page protection

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,19 +1,8 @@
-import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
-export const load: LayoutServerLoad = async (event) => {
-	const user = event.locals.user as { email: string; id: string; role: string } | null;
-	if (user) {
-		switch (user.role) {
-			case 'lgu':
-				throw redirect(302, '/lgu/product');
-			case 'admin':
-				throw redirect(302, '/admin/lgu');
-			case 'user':
-				throw redirect(302, '/landingpage');
-			default:
-				throw redirect(302, '/login');
-		}
-	}
-	return {};
+export const load: LayoutServerLoad = async ({ locals }) => {
+	return {
+		user: locals.user
+	};
 };
+
