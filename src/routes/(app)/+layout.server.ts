077efd5eb81227707f39d01856f2e53d0646b832
Chan10012002 <christian.obispo.5494@gmail.com>
2025-09-03 23:46:44 +0800

    fix:security

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -3,19 +3,17 @@ import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async (event) => {
 	const user = event.locals.user as { email: string; id: string; role: string } | null;
-
-	if (!user) {
-		throw redirect(302, '/login');
+	if (user) {
+		switch (user.role) {
+			case 'lgu':
+				throw redirect(302, '/lgu/product');
+			case 'admin':
+				throw redirect(302, '/admin/lgu');
+			case 'user':
+				throw redirect(302, '/landingpage');
+			default:
+				throw redirect(302, '/login');
+		}
 	}
-
-	switch (user.role) {
-		case 'lgu':
-			throw redirect(302, '/lgu/product');
-		case 'admin':
-			throw redirect(302, '/admin/lgu');
-		case 'user':
-			throw redirect(302, '/landingpage');
-		default:
-			throw redirect(302, '/login');
-	}
+	return {};
 };
